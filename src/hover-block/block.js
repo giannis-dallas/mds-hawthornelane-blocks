f037a1383582d658c0b8e2a43f2ee34271154f07
unknown <giannisdallas81@hotmail.com>
2019-03-08 19:26:34 +0200

    commit

---
+++ src/hover-block/block.js
@@ -0,0 +1,150 @@
+/**
+ * BLOCK: mds-hawthornelane
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+//  Import CSS.
+import './style.scss';
+import './editor.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+
+const { MediaPlaceholder, RichText } = wp.editor;
+
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'cgb/block-mds-hawthornelane-hover', {
+	title: __( 'Hover Effect' ), // Block title.
+	icon: 'format-image', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	keywords: [
+		__( 'hover effect' ),
+		__( 'CGB Example' ),
+		__( 'create-guten-block' ),
+	],
+
+	attributes: {
+        title: {
+            type: 'string',
+            source: 'html',
+			selector: 'text-title',
+		},
+		imageSRC:{
+			type: 'string',
+			source: 'attribute',
+			selector: 'img',
+			attribute: 'src'
+		},
+		imageClass:{
+			type: 'string',
+			source: 'attribute',
+			selector: 'img',
+			attribute: 'className'
+		},
+		desc: {
+			type: 'string',
+            source: 'html',
+			selector: 'text-desc',
+		}
+    },
+
+	edit( { attributes, setAttributes, className } ) {
+
+		let {imageSRC,title,desc} = attributes;
+
+		function selectImage(e) {
+			console.log(e);
+			setAttributes({
+				imageSRC: e.url
+			})
+		}
+
+		function onChangeTitle(value) {
+			console.log(value);
+			setAttributes({
+				title: value
+			})
+		}
+
+		function onChangeDesc(value) {
+			console.log(value);
+			setAttributes({
+				desc: value
+			})
+		}
+
+		return (
+			<div className={ className }>				
+				<MediaPlaceholder
+					onSelect = {selectImage}
+					allowedTypes = { [ 'image' ] }
+					multiple = { false }
+					labels = { { title: 'The Image' } }
+				/>
+				<label>Add the title</label>
+				<RichText
+					format="string"
+					className= "rich-text text-title"
+					formattingControls={ [] }
+					onChange={ onChangeTitle }
+					value={ title }
+				/>
+				<label>Add the description</label>
+				<RichText
+					format="string"
+					className= "rich-text text-desc"
+					formattingControls={ [] }
+					onChange={ onChangeDesc }
+					value={ desc }
+				/>
+				
+				
+				<div className="hover-effect">
+
+					<figure className="effect-bubba">
+						{imageSRC && <img src={imageSRC} alt="img"/>}
+						<figcaption>
+							<h2>{attributes.title}</h2>
+							<p><RichText.Content value={ desc } /></p>
+						</figcaption>			
+					</figure>
+
+				</div>
+			</div>
+		);
+	},
+
+	save( {attributes,className} ) {
+
+		const {imageSRC, title, desc} = attributes;
+		return (
+			<div>
+				<div className="hover-effect">
+
+					<figure className="effect-bubba">
+						{imageSRC && <img src={imageSRC} className="wp-image-00" alt="img"/>}
+						<figcaption>
+							<h2>{title}</h2>
+							<p><RichText.Content value={ desc } /></p>
+						</figcaption>			
+					</figure>
+
+				</div>
+			</div>
+		);
+	},
+} );
